3c229c5021f97696277d4f4a7e7f837bd3c91afc
cong tran <83268066+congtran18@users.noreply.github.com>
2021-05-30 18:59:15 +0700

    Update StartupScreen.js
    t mới thêm cơ chế cache dữ liệu mn xem nha

--- screens/StartupScreen.js
+++ screens/StartupScreen.js
@@ -6,6 +6,30 @@ import { useDispatch } from "react-redux";
 import * as authActions from "../store/actions/auth";
 
 const StartupScreen = (props) => {
+    const dispatch = useDispatch();
+  useEffect(() => {
+    const tryLogin = async () => {
+      const userData = await AsyncStorage.getItem("userData");
+      if (!userData) {
+        props.navigation.navigate("Auth");
+        return;
+      }
+      const transformedData = JSON.parse(userData);
+      const { token, userId, expiryDate } = transformedData;
+      const expirationDate = new Date(expiryDate);
+
+      if (expirationDate <= new Date() || !token || !userId) {
+        props.navigation.navigate("Auth");
+        return;
+      }
+
+      const expirationTime = expirationDate.getTime() - new Date().getTime;
+
+      props.navigation.navigate("Shop");
+      dispatch(authActions.authenticate(userId, token, expirationTime));
+    };
+    tryLogin();
+  }, [dispatch]);
 
 
   return (
