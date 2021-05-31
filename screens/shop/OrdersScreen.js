fb146c198e4cfc6b3ee58973742669eea006c808
cong tran <83268066+congtran18@users.noreply.github.com>
2021-05-31 23:16:20 +0700

    Update OrdersScreen.js

--- screens/shop/OrdersScreen.js
+++ screens/shop/OrdersScreen.js
@@ -15,9 +15,37 @@ import OrderItem from "../../components/shop/OrderItem";
 import * as ordersActions from "../../store/actions/orders";
 
 const OrdersScreen = (props) => {
+  const [isLoading, setIsLoading] = useState(false);
+
+  const orders = useSelector((state) => state.orders.orders);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    setIsLoading(true);
+    dispatch(ordersActions.fetchOrders()).then(() => {
+      setIsLoading(false);
+    });
+  }, [dispatch]);
+
+  if (isLoading) {
+    return (
+      <View style={styles.centered}>
+        <ActivityIndicator size="large" color={Colors.primary} />
+      </View>
+    );
+  }
+
+  if (orders.length === 0) {
+    return (
+      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+        <Text>No orders found, maybe start creating some?</Text>
+      </View>
+    );
+  }
 
   return (
     <FlatList
+      data={orders}
       keyExtractor={(item) => item.id}
       renderItem={(itemData) => (
         <OrderItem
