cec9ec9e96071c64a3316174067d8ef8811dca21
cong tran <83268066+congtran18@users.noreply.github.com>
2021-05-31 15:24:11 +0700

    Update NavigationContainer.js
    Thêm cơ chế cache data cho NavigationContainer.js

--- navigation/NavigationContainer.js
+++ navigation/NavigationContainer.js
@@ -8,7 +8,13 @@ const NavigationContainer = (props) => {
   const navRef = useRef();
   const isAuth = useSelector((state) => !!state.auth.token);
 
-
+    useEffect(() => {
+    if (!isAuth) {
+      navRef.current.dispatch(
+        NavigationActions.navigate({ routeName: "Auth" })
+      );
+    }
+  }, [isAuth]);
 
   return <ShopNavigator ref={navRef} />;
 };
