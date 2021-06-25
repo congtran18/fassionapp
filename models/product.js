f2fab8a7f55605d3810cd3664deb46e14828503d
cong tran <83268066+congtran18@users.noreply.github.com>
2021-06-25 10:50:51 +0700

    Update product.js models

--- models/product.js
+++ models/product.js
@@ -1,12 +1,14 @@
 class Product {
-  constructor(id, ownerId, title, imageUrl, description, price, type) {
+  constructor(id, ownerId, title, imageUrl, description, pricereal, pricesale, type, tinhtrang) {
     this.id = id;
     this.ownerId = ownerId;
     this.title = title;
     this.imageUrl = imageUrl;
     this.description = description;
-    this.price = price;
+    this.pricereal = pricereal;
+    this.pricesale = pricesale;
     this.type = type;
+    this.tinhtrang = tinhtrang;
   }
 }
 
